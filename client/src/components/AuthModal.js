19239e5c24472b17142d3a90a7c922bfd884ceec
Taro Serigano <taroserigano@gmail.com>
2023-02-13 09:18:03 -0600

    Update AuthModal.js

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useCookies } from 'react-cookie'
 
-
+// add JWT token on AuthToken cookie 
 const AuthModal = ({ setShowModal,  isSignUp }) => {
     const [email, setEmail] = useState(null)
     const [password, setPassword] = useState(null)
